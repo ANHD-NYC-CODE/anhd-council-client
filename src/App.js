029fd3158e32aa58d9e45221e774a26beca28936
0xstarcat <xxStarcat@gmail.com>
2019-01-29 21:38:16 -0500

    add leaflet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Header from './navigation/Header'
-
+import Map from './map/Map'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Map />
       </div>
     )
   }
---
+++ src/map/Map.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Map, TileLayer } from 'react-leaflet'
+
+import './map.scss'
+
+const center = [40.71, -73.98]
+
+export default class VectorLayersExample extends Component {
+  render() {
+    return (
+      <Map
+        doubleClickZoom={false}
+        id="leaflet-map"
+        minZoom={13}
+        maxZoom={20}
+        ref={this.props.mapRef}
+        zoomControl={false}
+        className="map"
+        center={center}
+        zoom={13}
+      >
+        <TileLayer
+          attribution="mapbox"
+          url="https://api.mapbox.com/styles/v1/starcat/cjrik99zl9yae2so1gjksdn9x/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3RhcmNhdCIsImEiOiJjamlpYmlsc28wbjlmM3FwbXdwaXozcWEzIn0.kLmWiUbmdqNLA1atmnTXXA"
+        />
+      </Map>
+    )
+  }
+}
