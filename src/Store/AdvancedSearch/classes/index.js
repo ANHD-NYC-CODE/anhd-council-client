24ab6e2ca66c37a52ce8f0cebb021137ab37bec7
0xstarcat <xxStarcat@gmail.com>
2019-03-02 22:21:40 -0500

    refactor class

--- src/Store/AdvancedSearch/classes/index.js
+++ src/Store/AdvancedSearch/classes/index.js
@@ -1,6 +1,67 @@
 import * as b from 'shared/constants/boundaries'
+import * as ht from 'shared/constants/housingTypes'
 
 export class Boundary {
+  constructor(type, id) {
+    this.setObject = this.setObject.bind(this)
+
+    this.setObject(type)
+
+    this._id = id
+  }
+
+  setObject(type) {
+    const object = b[Object.keys(b).find(obj => b[obj].constant === type)]
+    if (object) {
+      this._object = object
+      this._queryName = this.object.queryName
+      this._name = this.object.name
+      this._constant = this.object.constant
+    } else {
+      throw `Pass either '${Object.keys(b)
+        .map(key => b[key].constant)
+        .join("' or '")}' as the first argument.`
+    }
+  }
+
+  get object() {
+    return this._object
+  }
+  get id() {
+    return this._id
+  }
+
+  get name() {
+    return this._name
+  }
+  get queryName() {
+    return this._queryName
+  }
+  get constant() {
+    return this._constant
+  }
+
+  set object(value) {
+    this.setObject(value)
+    this.id = undefined // Clear the ID to avoid boundary/id mismatches
+  }
+
+  set id(value) {
+    this._id = value
+  }
+
+  set name(value) {
+    this._name = value
+  }
+  set queryName(value) {
+    this._queryName = value
+  }
+  set constant(value) {
+    this._constant = value
+  }
+}
+
+export class HousingType {
   constructor(type, id) {
     if (type === b.COUNCILBOUNDARY.constant) {
       this._object = b.COUNCILBOUNDARY
---
+++ src/shared/constants/housingTypes.js
@@ -0,0 +1,29 @@
+export const RENTSTABILZED = {
+  name: 'Rent Stabilized',
+  queryName: 'rs',
+  constant: 'RENT_STABILIZED',
+}
+
+export const RENTREGULATED = {
+  name: 'Rent Regulated',
+  queryName: 'rr',
+  constant: 'RENT_REGULATED',
+}
+
+export const SMALLHOMES = {
+  name: 'Small Homes',
+  queryName: 'sh',
+  constant: 'SMALL_HOMES',
+}
+
+export const MARKETRATE = {
+  name: 'Market Rate',
+  queryName: 'mr',
+  constant: 'MARKET_RATE',
+}
+
+export const PUBLICHOUSING = {
+  name: 'Public Housing',
+  queryName: 'ph',
+  constant: 'PUBLIC_HOUSING',
+}
