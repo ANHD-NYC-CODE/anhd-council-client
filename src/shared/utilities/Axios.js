b2a6d38a08f35d741340b9943ccdff19a110a093
0xstarcat <xxStarcat@gmail.com>
2019-03-07 01:34:58 -0500

    increase axios timeout, add language modules to housing types

--- src/shared/utilities/Axios.js
+++ src/shared/utilities/Axios.js
@@ -4,7 +4,7 @@ import { handleCompletedRequest } from 'Store/Loading/actions'
 
 export const Axios = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  timeout: 30000,
+  timeout: 60000,
   headers: { 'Content-type': 'application/json' },
 })
 
