51678abf4ec86ecbb0b52edffbacc379af7f644f
0xstarcat <xxStarcat@gmail.com>
2019-03-08 00:37:12 -0500

    fix that page overflow bug

--- src/Layout/Header/index.js
+++ src/Layout/Header/index.js
@@ -1,33 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Row, Nav, Col } from 'react-bootstrap'
+import { Navbar, Row, Nav, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Header = props => {
   return (
-    <div className="header">
-      <Row>
-        <Col sm={12} md={6}>
-          <Nav variant="tabs" defaultActiveKey="/buildings">
-            <Nav.Item>
-              <Nav.Link as="li">
-                <Link to="/buildings">Building Lookup</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as="li">
-                <Link to="/districts">District Alerts</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link as="li">
-                <Link to="/search">Advanced Search</Link>
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Col>
-      </Row>
-    </div>
+    <Navbar className="sub-header">
+      <Col sm={12} md={6}>
+        <Nav variant="tabs" defaultActiveKey="/buildings">
+          <Nav.Item>
+            <Nav.Link as="li">
+              <Link to="/buildings">Building Lookup</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as="li">
+              <Link to="/districts">District Alerts</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link as="li">
+              <Link to="/search">Advanced Search</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Col>
+    </Navbar>
   )
 }
 
