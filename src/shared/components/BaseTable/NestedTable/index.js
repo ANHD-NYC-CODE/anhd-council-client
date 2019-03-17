b49f8a4513eaf46c042b75285892aaefcd5ff5ff
0xstarcat <xxStarcat@gmail.com>
2019-03-17 02:04:17 -0400

    clear em filters

--- src/shared/components/BaseTable/NestedTable/index.js
+++ src/shared/components/BaseTable/NestedTable/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import BootstrapTable from 'react-bootstrap-table-next'
 import filterFactory from 'react-bootstrap-table2-filter'
+import TableAlert from 'shared/components/BaseTable/TableAlert'
 
 import BaseTable from 'shared/components/BaseTable'
 import { getTableColumns, getKeyField } from 'shared/models/tables'
@@ -13,12 +14,13 @@ class NestedTable extends BaseTable {
   }
 
   render() {
+    const columns = getTableColumns(this.props.nestedModelKey, this.setExpandedContent, null, this.constructFilter)
     return (
       <Card bg="light" border="secondary" className="nested-table">
         <BootstrapTable
           keyField={getKeyField(this.props.nestedModelKey)}
           bootstrap4
-          columns={getTableColumns(this.props.nestedModelKey, this.setExpandedContent)}
+          columns={columns}
           data={this.props.data}
           condensed
           filter={filterFactory()}
@@ -26,6 +28,16 @@ class NestedTable extends BaseTable {
           expandRow={this.expandRow()}
           tabIndexCell
           rowClasses={'table-row--collapsed'}
+          noDataIndication={
+            <TableAlert
+              textType="text-dark"
+              variant="warning"
+              message={"There's nothing here."}
+              buttonText="Clear Filters"
+              buttonVariant="secondary"
+              action={() => Object.keys(this.filters).forEach(key => this.filters[key](''))}
+            />
+          }
         />
       </Card>
     )
