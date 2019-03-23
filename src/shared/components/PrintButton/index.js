0916c9c617fa4fa3e709ac715d52dceab3cf5548
0xstarcat <xxStarcat@gmail.com>
2019-03-22 22:50:01 -0400

    add PrintButton component

---
+++ src/shared/components/PrintButton/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import LayoutContext from 'Layout/LayoutContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPrint } from '@fortawesome/free-solid-svg-icons'
+
+import { Button } from 'react-bootstrap'
+const PrintButton = props => {
+  if (window.innerWidth < 768) return
+  return (
+    <LayoutContext.Consumer>
+      {layout => (
+        <Button onClick={() => layout.togglePrint(props.title)}>
+          <FontAwesomeIcon icon={faPrint} />
+          <span> Print</span>
+        </Button>
+      )}
+    </LayoutContext.Consumer>
+  )
+}
+
+PrintButton.propTypes = {
+  title: PropTypes.string,
+}
+
+export default PrintButton
