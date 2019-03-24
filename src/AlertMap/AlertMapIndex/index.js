7454d7ccdfb9744af0fd3d35f96b9f1111fb62db
0xstarcat <xxStarcat@gmail.com>
2019-03-24 11:51:16 -0400

    main page spacers + text styles

--- src/AlertMap/AlertMapIndex/index.js
+++ src/AlertMap/AlertMapIndex/index.js
@@ -12,7 +12,7 @@ const AlertMapIndex = props => {
         <Col className="touch-left padding-xs-sm-0" xs={12} md={6} lg={5}>
           <IntroductionBlock />
         </Col>
-        <Col className="px-4 py-6" xs={12} md={6} lg={7}>
+        <Col className="px-md-4 py-3 py-md-6" xs={12} md={6} lg={7}>
           <Row>
             <Col>
               <p className="text-muted font-weight-bold">Select a district to begin.</p>
