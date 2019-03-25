a60f76db184ecb987be4af7fea0905f18d0f507d
0xstarcat <xxStarcat@gmail.com>
2019-03-25 11:40:24 -0400

    add spinner loaders

---
+++ src/shared/components/Loaders/SpinnerLoader/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './style.scss'
+const SpinnerLoader = props => {
+  return (
+    <div className="spinner-loader">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+}
+
+SpinnerLoader.propTypes = {}
+
+export default SpinnerLoader
