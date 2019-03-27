ae21e00b1c43967dc5d56e14ed0143911f58fa5a
0xstarcat <xxStarcat@gmail.com>
2019-03-27 18:50:13 -0400

    fix meta

--- src/shared/models/modals.js
+++ src/shared/models/modals.js
@@ -1,66 +1,102 @@
 export const infoModals = {
   GEOGRAPHY_HOUSING_TYPE_RS: {
     title: 'Rent Stabilized Housing',
-    body: 'Lorem ipsum...',
+    body:
+      'An apartment may be regulated under New York State’s system of Rent Stabilization if it is in a property that was built before 1974 and has six or more units. All properties in this report included at least one rent stabilized unit beginning in 2007, the first year for which we have data. We base stabilized unit numbers on property tax bills from the NYC Department of Finance. Missing or fluctuating numbers may be due to missing/incorrect registration by the owner or inconsistencies in property tax documentation.',
+    sourceText: 'taxbills.nyc',
+    sourceUrl: 'https://github.com/talos/nyc-stabilization-unit-counts',
   },
   GEOGRAPHY_HOUSING_TYPE_RR: {
     title: 'Rent Regulated Housing',
-    body: 'Lorem ipsum...',
+    body:
+      'These properties are subject to a regulatory agreement at the City, State, or Federal level. This includes tax incentives and financing that determines affordability levels. ',
+    sourceText: 'New York University Furman Center’s CoreData.nyc',
+    sourceUrl: 'http://coredata.nyc/',
   },
   GEOGRAPHY_HOUSING_TYPE_SH: {
     title: 'Small Homes',
-    body: 'Lorem ipsum...',
+    body: 'We consider small homes to be any property with four or fewer dwelling units.',
+    sourceText: 'NYC Department of City Planning',
+    sourceUrl: 'https://www1.nyc.gov/site/planning/data-maps/open-data.page#pluto',
   },
   GEOGRAPHY_HOUSING_TYPE_MR: {
     title: 'Market Rate',
-    body: 'Lorem ipsum...',
+    body:
+      'We consider market rate properties to be buildings that are not covered under either New York State rent stabilization code or any regulatory agreement governing apartment affordability.',
+    sourceText: 'NYC Department of City Planning',
+    sourceUrl: 'https://www1.nyc.gov/site/planning/data-maps/open-data.page#pluto',
   },
   GEOGRAPHY_HOUSING_TYPE_PH: {
-    title: 'Market Rate',
-    body: 'Lorem ipsum...',
+    title: 'Public Housing',
+    body:
+      'We consider public housing properties to be any building operated by the New York City Housing Authority (NYCHA)',
+    sourceText: 'NYCHA Property Directory via JustFixNYC nycha scraper',
+    sourceUrl: 'https://github.com/JustFixNYC/nycha-scraper',
   },
   HPD_VIOLATION: {
     title: 'HPD Violations',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   HPD_COMPLAINT: {
     title: 'HPD Complaints',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   DOB_VIOLATION: {
     title: 'DOB Violations',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   DOB_COMPLAINT: {
     title: 'DOB Complaints',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   DOB_ISSUED_PERMIT: {
     title: 'DOB Issued Permits',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   DOB_LEGACY_FILED_PERMIT: {
     title: 'DOB Filed Permits',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   ECB_VIOLATION: {
     title: 'ECB Violations',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   ACRIS_REAL_MASTER: {
     title: 'ACRIS Sales',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   LISPENDEN: {
     title: 'Foreclosures',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   EVICTION: {
     title: 'Evictions',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
   HOUSING_LITIGATION: {
     title: 'Housing Litigations',
     body: 'Lorem ipsum...',
+    sourceText: '',
+    sourceUrl: '',
   },
 }
