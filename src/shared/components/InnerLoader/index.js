ab83a785660de0b2254743d26deca68e897dfbd0
0xstarcat <xxStarcat@gmail.com>
2019-03-28 02:51:49 -0400

    loading screen

--- src/shared/components/InnerLoader/index.js
+++ src/shared/components/InnerLoader/index.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types'
 import './style.scss'
 const InnerLoader = props => {
   return (
-    <div className="inner-loader">
+    <div className={`inner-loader ${props.className}`} style={{ height: props.height }}>
       <div className="location_indicator" />
     </div>
   )
 }
 
-InnerLoader.propTypes = {}
+InnerLoader.defaultProps = {
+  height: '200px',
+}
+
+InnerLoader.propTypes = {
+  backgroundColor: PropTypes.string,
+  height: PropTypes.string,
+}
 
 export default InnerLoader
