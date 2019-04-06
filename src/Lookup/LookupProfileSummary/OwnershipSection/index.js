b04c1f50768b49a5d976e5a9fa6aa56684143c61
0xstarcat <xxStarcat@gmail.com>
2019-04-06 11:33:37 -0400

    more styles

--- src/Lookup/LookupProfileSummary/OwnershipSection/index.js
+++ src/Lookup/LookupProfileSummary/OwnershipSection/index.js
@@ -10,54 +10,54 @@ import TableConfig from 'shared/classes/TableConfig'
 
 const OwnershipSection = props => {
   return (
-    <Row className="property-section property-summary-body my-4">
-      <Col>
-        <Row>
-          <Col>
-            <h5 className="text-dark">Ownership</h5>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <label className="profile-summary-body__label">Owner Name:&nbsp;</label>
-            <span className="profile-summary-body__value">{props.profile.ownername}</span>
-          </Col>
-        </Row>
-        <Row className="my-2">
-          <Col>
-            <BaseLink
-              className="lookup-links__link"
-              href={`https://whoownswhat.justfix.nyc/bbl/${props.profile.bbl}`}
-              text="View more at Who Owns What?"
-            />
-          </Col>
-        </Row>
-        <Row className="my-4">
-          <Col>
-            <Row>
-              <Col>
-                <h5>HPD Registrations</h5>
-              </Col>
-            </Row>
-            <Row>
-              {props.profile.hpdregistrations.length ? (
-                <BaseTable
-                  classes="fluid-table"
-                  wrapperClasses="text-dark property-summary__table"
-                  columns={getTableColumns('HPD_REGISTRATION')}
-                  dispatch={props.dispatch}
-                  records={props.profile.hpdregistrations}
-                  request={props.request}
-                  tableConfig={new TableConfig({ resourceConstant: 'HPD_REGISTRATION' })}
-                />
-              ) : (
-                <Col className="text-dark-gray text-center font-weight-bold my-4">No HPD Registrations Found</Col>
-              )}
-            </Row>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+    <div>
+      <Row className="property-section property-summary-body my-4">
+        <Col>
+          <Row>
+            <Col>
+              <h5 className="text-dark">Ownership</h5>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <label className="profile-summary-body__label">Owner Name:&nbsp;</label>
+              <span className="profile-summary-body__value">{props.profile.ownername}</span>
+            </Col>
+          </Row>
+          <Row className="my-2">
+            <Col>
+              <BaseLink
+                className="lookup-links__link"
+                href={`https://whoownswhat.justfix.nyc/bbl/${props.profile.bbl}`}
+                text="View more at Who Owns What?"
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="my-4">
+        <Col>
+          <Row>
+            <h5 className="text-light">HPD Registrations</h5>
+          </Row>
+          <Row>
+            {props.profile.hpdregistrations.length ? (
+              <BaseTable
+                classes="fluid-table"
+                wrapperClasses="text-dark property-summary__table"
+                columns={getTableColumns('HPD_REGISTRATION')}
+                dispatch={props.dispatch}
+                records={props.profile.hpdregistrations}
+                request={props.request}
+                tableConfig={new TableConfig({ resourceConstant: 'HPD_REGISTRATION' })}
+              />
+            ) : (
+              <Col className="text-dark-gray text-center font-weight-bold my-4">No HPD Registrations Found</Col>
+            )}
+          </Row>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
