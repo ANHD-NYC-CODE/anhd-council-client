240ebf418d859ba3ada0ee2ffaa24fd684d6025a
0xstarcat <xxStarcat@gmail.com>
2019-05-05 16:07:37 -0400

    renable GA

--- src/shared/constants/config.js
+++ src/shared/constants/config.js
@@ -1,6 +1,6 @@
 // Feature Flags
 export const ENABLE_PRINT = false // Shows print buttons
-export const ENABLE_GOOGLE_ANALYTICS = false
+export const ENABLE_GOOGLE_ANALYTICS = true
 
 // Generic
 export const CONTACT_EMAIL = 'dapadmin@anhd.org'
