b76335a476384acf63fedf262ae84394c30b7a53
0xstarcat <xxStarcat@gmail.com>
2019-05-05 13:59:04 -0400

    disabling analytics for stress test

--- src/shared/constants/config.js
+++ src/shared/constants/config.js
@@ -1,6 +1,6 @@
 // Feature Flags
 export const ENABLE_PRINT = false // Shows print buttons
-export const ENABLE_GOOGLE_ANALYTICS = true
+export const ENABLE_GOOGLE_ANALYTICS = false
 
 // Generic
 export const CONTACT_EMAIL = 'dapadmin@anhd.org'
