cc5136b62434061094fa68e71ea3fa002253653d
0xstarcat <xxStarcat@gmail.com>
2019-10-05 11:09:18 -0400

    undo

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -6,16 +6,14 @@ import { getUserStorageData } from 'shared/utilities/storageUtils'
 import App from 'App'
 import ReactGA from 'react-ga'
 import * as c from 'shared/constants'
-
-// console.log(c.ENABLE_GOOGLE_ANALYTICS, process.env.NODE_ENV)
-// if (c.ENABLE_GOOGLE_ANALYTICS) {
-ReactGA.initialize('UA-137162338-1', {
-  debug: process.env.NODE_ENV === 'development',
-  testMode: process.env.NODE_ENV === 'development',
-  anonymizeIp: true, // Removes last 3 digits from IP
-  cookieDomain: false,
-})
-// }
+if (c.ENABLE_GOOGLE_ANALYTICS) {
+  ReactGA.initialize('UA-137162338-1', {
+    debug: process.env.NODE_ENV === 'development',
+    testMode: process.env.NODE_ENV === 'development',
+    anonymizeIp: true, // Removes last 3 digits from IP
+    cookieDomain: false,
+  })
+}
 ReactGA.pageview(window.location.pathname + window.location.search)
 
 import 'react-toastify/dist/ReactToastify.css'
