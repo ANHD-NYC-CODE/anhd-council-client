9fdc2375ff5e368c5a63e98486d7ab4281b58faa
0xstarcat <xxStarcat@gmail.com>
2019-11-15 16:20:59 -0500

    add LLC to preserve uppercase list

--- src/shared/utilities/languageUtils.js
+++ src/shared/utilities/languageUtils.js
@@ -1,11 +1,17 @@
 export const capitalizeWords = string => {
   if (!string) return ''
-  return string
-    .split(' ')
-    .map(word => {
-      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-    })
-    .join(' ')
+  return preserveUppercaseTerms(
+    string
+      .split(' ')
+      .map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      })
+      .join(' ')
+  )
+}
+
+export const preserveUppercaseTerms = string => {
+  return string.replace(/\bllc\b/i, 'LLC')
 }
 
 export const splitCamelCase = string => {
