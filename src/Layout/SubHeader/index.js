d417763cb25f1bd7247ba48043bb3920031a389d
0xstarcat <xxStarcat@gmail.com>
2019-12-09 21:50:30 -0500

    update black/gray900 color, update subheader--top

--- src/Layout/SubHeader/index.js
+++ src/Layout/SubHeader/index.js
@@ -22,147 +22,211 @@ class SubHeader extends React.Component {
 
   render() {
     return (
-      <Container fluid>
-        <Row className="sub-header touch-left">
-          <Col className="d-flex justify-content-center" xs={12} md={4} lg={3}>
-            <Navbar.Brand as="li">
+      <Nav className="sub-header">
+        <Navbar className="sub-header__wrapper--top touch-left">
+          <Nav className="mr-auto">
+            <Navbar.Brand className="mr-auto">
               <Link to="/">
-                <img
-                  src={logo}
-                  className="sub-header__logo d-inline-block align-top"
-                  alt="Displacement Alert Portal Logo"
-                />
+                <img src={logo} className="sub-header__logo" alt="Displacement Alert Portal Logo" />
               </Link>
             </Navbar.Brand>
-          </Col>
-          <Col xs={12} md={8} lg={9}>
-            <Row className="sub-header__wrapper--top">
-              <Col xs={12} md={{ span: 10, offset: 2 }}>
-                <Row className="sub-header__auth-row flex-column flex-sm-row">
-                  <div className="d-flex">
-                    <Col>
-                      <Nav.Item
-                        onClick={e => {
-                          e.preventDefault()
-                          this.props.modal.setModal({
-                            modalComponent: BugReportModal,
-                            modalProps: {
-                              size: 'lg',
-                            },
-                          })
-                        }}
-                      >
-                        <a className="text-secondary" href="#">
-                          Report a bug
-                        </a>
-                      </Nav.Item>
-                    </Col>
-                    <Col>
-                      <Nav.Item>
-                        <BaseLink
-                          href="https://forms.gle/EAUkzgsAkHn8NgbTA"
-                          className="text-secondary"
-                          text="Feedback"
-                        />
-                      </Nav.Item>
-                    </Col>
-                    {this.props.auth.user ? (
-                      <div className="d-flex">
-                        <Col>
-                          <Nav.Item className="text-secondary">{this.props.auth.user.username}</Nav.Item>
-                        </Col>
-                        <Col>
-                          <Nav.Item
-                            onClick={e => {
-                              e.preventDefault()
-                              this.props.auth.logoutUser()
-                            }}
-                          >
-                            <a className="text-secondary" href="#">
-                              Logout
-                            </a>
-                          </Nav.Item>
-                        </Col>
-                      </div>
-                    ) : (
-                      <Col>
-                        <Nav.Item
-                          onClick={e => {
-                            e.preventDefault()
-                            this.props.modal.setModal({
-                              modalComponent: LoginModal,
-                              modalProps: {
-                                modalFooter: <LoginModalFooter modal={this.props.modal} />,
-                              },
-                            })
-                          }}
-                        >
-                          <a className="text-secondary" href="#" id="nav-login-button">
-                            Login / Sign up
-                          </a>
-                        </Nav.Item>
-                      </Col>
-                    )}
-                  </div>
-                </Row>
-              </Col>
-            </Row>
-            <Row className="sub-header__wrapper--bottom">
-              <Col xs={12}>
-                <Nav
-                  className="sub-header__nav-row flex-column flex-sm-row"
-                  variant="pills"
-                  activeKey={this.state.key}
-                  onSelect={key => this.setState({ key })}
+          </Nav>
+          <Nav className="sub-header__wrapper--top__links">
+            <Nav.Item
+              onClick={e => {
+                e.preventDefault()
+                this.props.modal.setModal({
+                  modalComponent: BugReportModal,
+                  modalProps: {
+                    size: 'lg',
+                  },
+                })
+              }}
+            >
+              <a className="text-gray-900" href="#">
+                Report a bug
+              </a>
+            </Nav.Item>
+            <Nav.Item>
+              <BaseLink href="https://forms.gle/EAUkzgsAkHn8NgbTA" className="text-gray-900" text="Feedback" />
+            </Nav.Item>
+            {this.props.auth.user ? (
+              <div className="d-flex">
+                <Nav.Item className="text-gray-900">{this.props.auth.user.username}</Nav.Item>
+                <Nav.Item
+                  onClick={e => {
+                    e.preventDefault()
+                    this.props.auth.logoutUser()
+                  }}
                 >
-                  <Link
-                    className={classnames('sub-header__nav-tab', {
-                      active: this.props.pathname.match(/(lookup|property)/),
-                    })}
-                    to={
-                      this.props.currentProperty
-                        ? addressResultToPath({
-                            bbl: this.props.currentProperty,
-                            bin: this.props.currentBuilding,
-                          })
-                        : '/lookup'
-                    }
-                  >
-                    <div className="sub-header__nav-tab--inner">
-                      <Nav.Item>Property Lookup</Nav.Item>
-                    </div>
-                  </Link>
-                  <Link
-                    className={classnames('sub-header__nav-tab', {
-                      active: this.props.pathname.match(/(map|council|community)/),
-                    })}
-                    to={`${
-                      this.props.currentGeographyType && this.props.currentGeographyId
-                        ? '/' + getGeographyPath(this.props.currentGeographyType) + '/' + this.props.currentGeographyId
-                        : '/map'
-                    }`}
-                  >
-                    <div className="sub-header__nav-tab--inner">
-                      <Nav.Item>District Dashboard</Nav.Item>
-                    </div>
-                  </Link>
-                  <Link
-                    to="/search"
-                    className={classnames('sub-header__nav-tab', {
-                      active: this.props.pathname.match(/(search)/),
-                    })}
-                  >
-                    <div className="sub-header__nav-tab--inner">
-                      <Nav.Item>Custom Search</Nav.Item>
-                    </div>
-                  </Link>
-                </Nav>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
+                  <a className="text-gray-900" href="#">
+                    Logout
+                  </a>
+                </Nav.Item>
+              </div>
+            ) : (
+              <Nav.Item
+                onClick={e => {
+                  e.preventDefault()
+                  this.props.modal.setModal({
+                    modalComponent: LoginModal,
+                    modalProps: {
+                      modalFooter: <LoginModalFooter modal={this.props.modal} />,
+                    },
+                  })
+                }}
+              >
+                <a className="text-gray-900" href="#" id="nav-login-button">
+                  Login / Sign up
+                </a>
+              </Nav.Item>
+            )}
+          </Nav>
+        </Navbar>
+      </Nav>
     )
+    // return (
+    //   <Container fluid>
+    //     <Row className="sub-header touch-left">
+    //       <Col className="d-flex justify-content-center" xs={12} md={4} lg={3}>
+    //         <Navbar.Brand as="li">
+    //           <Link to="/">
+    //             <img
+    //               src={logo}
+    //               className="sub-header__logo d-inline-block align-top"
+    //               alt="Displacement Alert Portal Logo"
+    //             />
+    //           </Link>
+    //         </Navbar.Brand>
+    //       </Col>
+    //       <Col xs={12} md={8} lg={9}>
+    //         <Row className="sub-header__wrapper--top">
+    //           <Col xs={12} md={{ span: 10, offset: 2 }}>
+    //             <Row className="sub-header__auth-row flex-column flex-sm-row">
+    //               <div className="d-flex">
+    //                 <Col>
+    //                   <Nav.Item
+    //                     onClick={e => {
+    //                       e.preventDefault()
+    //                       this.props.modal.setModal({
+    //                         modalComponent: BugReportModal,
+    //                         modalProps: {
+    //                           size: 'lg',
+    //                         },
+    //                       })
+    //                     }}
+    //                   >
+    //                     <a className="text-gray-900" href="#">
+    //                       Report a bug
+    //                     </a>
+    //                   </Nav.Item>
+    //                 </Col>
+    //                 <Col>
+    //                   <Nav.Item>
+    //                     <BaseLink
+    //                       href="https://forms.gle/EAUkzgsAkHn8NgbTA"
+    //                       className="text-gray-900"
+    //                       text="Feedback"
+    //                     />
+    //                   </Nav.Item>
+    //                 </Col>
+    //                 {this.props.auth.user ? (
+    //                   <div className="d-flex">
+    //                     <Col>
+    //                       <Nav.Item className="text-gray-900">{this.props.auth.user.username}</Nav.Item>
+    //                     </Col>
+    //                     <Col>
+    //                       <Nav.Item
+    //                         onClick={e => {
+    //                           e.preventDefault()
+    //                           this.props.auth.logoutUser()
+    //                         }}
+    //                       >
+    //                         <a className="text-gray-900" href="#">
+    //                           Logout
+    //                         </a>
+    //                       </Nav.Item>
+    //                     </Col>
+    //                   </div>
+    //                 ) : (
+    //                   <Col>
+    //                     <Nav.Item
+    //                       onClick={e => {
+    //                         e.preventDefault()
+    //                         this.props.modal.setModal({
+    //                           modalComponent: LoginModal,
+    //                           modalProps: {
+    //                             modalFooter: <LoginModalFooter modal={this.props.modal} />,
+    //                           },
+    //                         })
+    //                       }}
+    //                     >
+    //                       <a className="text-gray-900" href="#" id="nav-login-button">
+    //                         Login / Sign up
+    //                       </a>
+    //                     </Nav.Item>
+    //                   </Col>
+    //                 )}
+    //               </div>
+    //             </Row>
+    //           </Col>
+    //         </Row>
+    //         <Row className="sub-header__wrapper--bottom">
+    //           <Col xs={12}>
+    //             <Nav
+    //               className="sub-header__nav-row flex-column flex-sm-row"
+    //               variant="pills"
+    //               activeKey={this.state.key}
+    //               onSelect={key => this.setState({ key })}
+    //             >
+    //               <Link
+    //                 className={classnames('sub-header__nav-tab', {
+    //                   active: this.props.pathname.match(/(lookup|property)/),
+    //                 })}
+    //                 to={
+    //                   this.props.currentProperty
+    //                     ? addressResultToPath({
+    //                         bbl: this.props.currentProperty,
+    //                         bin: this.props.currentBuilding,
+    //                       })
+    //                     : '/lookup'
+    //                 }
+    //               >
+    //                 <div className="sub-header__nav-tab--inner">
+    //                   <Nav.Item>Property Lookup</Nav.Item>
+    //                 </div>
+    //               </Link>
+    //               <Link
+    //                 className={classnames('sub-header__nav-tab', {
+    //                   active: this.props.pathname.match(/(map|council|community)/),
+    //                 })}
+    //                 to={`${
+    //                   this.props.currentGeographyType && this.props.currentGeographyId
+    //                     ? '/' + getGeographyPath(this.props.currentGeographyType) + '/' + this.props.currentGeographyId
+    //                     : '/map'
+    //                 }`}
+    //               >
+    //                 <div className="sub-header__nav-tab--inner">
+    //                   <Nav.Item>District Dashboard</Nav.Item>
+    //                 </div>
+    //               </Link>
+    //               <Link
+    //                 to="/search"
+    //                 className={classnames('sub-header__nav-tab', {
+    //                   active: this.props.pathname.match(/(search)/),
+    //                 })}
+    //               >
+    //                 <div className="sub-header__nav-tab--inner">
+    //                   <Nav.Item>Custom Search</Nav.Item>
+    //                 </div>
+    //               </Link>
+    //             </Nav>
+    //           </Col>
+    //         </Row>
+    //       </Col>
+    //     </Row>
+    //   </Container>
+    // )
   }
 }
 
