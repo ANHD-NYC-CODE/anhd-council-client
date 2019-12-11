81d66fa19f038eaad4f42a0cb866e25379c47b0d
0xstarcat <xxStarcat@gmail.com>
2019-12-10 19:26:39 -0500

    footer style

--- src/Layout/Footer/index.js
+++ src/Layout/Footer/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Row, Col } from 'react-bootstrap'
 // We hope this will lead to better decisions on what to
 // prioritize and how to improve the site.
 import PrivacyPolicyModal from 'shared/components/modals/PrivacyPolicyModal'
 import ModalContext from 'Modal/ModalContext'
 import { fireAnalyticsModalOpenEvent } from 'Store/Analytics/actions'
+
+import './style.scss'
 
 class Footer extends React.Component {
   constructor(props) {
@@ -19,32 +21,36 @@ class Footer extends React.Component {
         {modal => {
           return (
             <footer className="footer">
-              <Navbar bg="dark" variant="light" expand="lg">
+              <Navbar className="footer-nav" expand="lg">
                 <div className="layout-width-wrapper">
-                  <p className="text-muted small">
-                    This site uses{' '}
-                    <a href="https://analytics.google.com" target="_blank" rel="noopener noreferrer">
-                      Google Analytics
-                    </a>{' '}
-                    to help us better understand how it's used.
-                    <a
-                      href="#"
-                      onClick={() => {
-                        this.props.dispatch(fireAnalyticsModalOpenEvent())
+                  <Row>
+                    <Col className="footer-text">
+                      <p className="text-black">
+                        This site uses{' '}
+                        <a href="https://analytics.google.com" target="_blank" rel="noopener noreferrer">
+                          Google Analytics
+                        </a>{' '}
+                        to help us better understand how it's used.
+                        <a
+                          href="#"
+                          onClick={() => {
+                            this.props.dispatch(fireAnalyticsModalOpenEvent())
 
-                        modal.setModal({
-                          modalComponent: PrivacyPolicyModal,
-                          modalProps: {
-                            size: 'lg',
-                          },
-                        })
-                      }}
-                    >
-                      {' '}
-                      Read more
-                    </a>{' '}
-                    about how we collect analytics.
-                  </p>
+                            modal.setModal({
+                              modalComponent: PrivacyPolicyModal,
+                              modalProps: {
+                                size: 'lg',
+                              },
+                            })
+                          }}
+                        >
+                          {' '}
+                          Read more
+                        </a>{' '}
+                        about how we collect analytics.
+                      </p>
+                    </Col>
+                  </Row>
                 </div>
               </Navbar>
             </footer>
