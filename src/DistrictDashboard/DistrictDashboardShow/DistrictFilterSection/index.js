de78b17d70097abcc85aeb5fb356952d1afc89eb
0xstarcat <xxStarcat@gmail.com>
2020-01-02 01:10:41 -0500

    update amount filters

--- src/DistrictDashboard/DistrictDashboardShow/DistrictFilterSection/index.js
+++ src/DistrictDashboard/DistrictDashboardShow/DistrictFilterSection/index.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import * as c from 'shared/constants'
-
 import { setAppState } from 'Store/AppState/actions'
 import { setDashboardCustomView, toggleSelectedAmountFilter } from 'Store/DashboardState/actions'
 import UserContext from 'Auth/UserContext'
 import AnnotatedResultFilterCard from 'DistrictDashboard/AnnotatedResultFilterCard'
-import ResultFilterError from 'shared/components/ResultFilterError'
 import { setHousingTypeResultFilter, setDashboardTableState } from 'Store/DashboardState/actions'
 
-import ModalContext from 'Modal/ModalContext'
-import LoginModal from 'shared/components/modals/LoginModal'
-import LoginModalFooter from 'shared/components/forms/LoginForm/LoginModalFooter'
 import { fireFilterSelectEvent } from 'Store/Analytics/actions'
 
 import './style.scss'
@@ -61,43 +55,17 @@ const DistrictFilterSection = props => {
             {props.dashboardState.resultFilters
               .filter(f => f.category === 'AMOUNT')
               .map((amountFilter, index) => {
-                return (amountFilter.resourceModel.resourceConstant === 'FORECLOSURE' ||
-                  amountFilter.resourceModel.resourceConstant === 'LISPENDEN') &&
-                  !auth.user ? (
-                  <ModalContext.Consumer>
-                    {modal => {
-                      return (
-                        <ResultFilterError
-                          amountFilter={amountFilter}
-                          ctaText={c.LOGIN_CTA}
-                          error={{ message: 'Please login to view.' }}
-                          errorAction={e => {
-                            e.preventDefault()
-                            modal.setModal({
-                              modalComponent: LoginModal,
-                              modalProps: {
-                                modalFooter: <LoginModalFooter modal={modal} />,
-                              },
-                            })
-                          }}
-                          requestLabel={amountFilter.label}
-                        />
-                      )
-                    }}
-                  </ModalContext.Consumer>
-                ) : (
-                  <div>
-                    <AnnotatedResultFilterCard
-                      auth={auth}
-                      key={`request-summary-${amountFilter.category}-${index}`}
-                      amountFilter={amountFilter}
-                      calculatedTotal={(props.dashboardState.resultFilterCalculations[index] || {}).length}
-                      disabled={props.customView || props.loading}
-                      dispatch={props.dispatch}
-                      selected={!props.customView && props.dashboardState.selectedFilters.includes(amountFilter)}
-                      handleClick={() => handleResultFilterClick(amountFilter)}
-                    />
-                  </div>
+                return (
+                  <AnnotatedResultFilterCard
+                    key={`request-summary-${amountFilter.category}-${index}`}
+                    auth={auth}
+                    amountFilter={amountFilter}
+                    calculatedTotal={(props.dashboardState.resultFilterCalculations[index] || {}).length}
+                    disabled={props.customView || props.loading}
+                    dispatch={props.dispatch}
+                    selected={!props.customView && props.dashboardState.selectedFilters.includes(amountFilter)}
+                    handleClick={() => handleResultFilterClick(amountFilter)}
+                  />
                 )
               })}
           </div>
