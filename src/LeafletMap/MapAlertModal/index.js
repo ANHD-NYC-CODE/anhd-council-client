c6f2e32393593341d1b375a7575402109c4ddbcd
0xstarcat <xxStarcat@gmail.com>
2020-01-02 14:12:04 -0500

    add mapmodal and switch baselayer

--- src/LeafletMap/MapAlertModal/index.js
+++ src/LeafletMap/MapAlertModal/index.js
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+
+import SpinnerLoader from 'shared/components/Loaders/SpinnerLoader'
 import { Button, Alert } from 'react-bootstrap'
 import { spaceEnterKeyDownHandler } from 'shared/utilities/accessibilityUtils'
 import './style.scss'
 
 const MapAlertModal = props => {
+  const [isLoading, toggleLoading] = useState(false)
+
+  const handleClick = e => {
+    e.preventDefault()
+    toggleLoading(true)
+    props.action()
+  }
+
   return (
     <div className="map-alert-modal">
       <Alert className="map-alert-modal__alert" variant={props.alertVariant}>
         <span className="map-alert-modal__text">{props.alertMessage}</span>
         <Button
+          className="btn-loader"
           variant="primary"
           size="sm"
-          onClick={props.action}
-          onKeyDown={e => spaceEnterKeyDownHandler(e, () => props.action())}
+          onClick={handleClick}
+          onKeyDown={e => spaceEnterKeyDownHandler(e, e => handleClick(e))}
         >
           {props.alertCta}
+          <div className="button-loader__container">{isLoading && <SpinnerLoader size="20px" />}</div>
         </Button>
       </Alert>
     </div>
