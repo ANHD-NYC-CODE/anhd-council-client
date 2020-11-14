8bc91d7e311c836fdf1905e0682409d6bdd58ee5
0xstarcat <xxStarcat@gmail.com>
2020-11-14 13:00:53 -0500

    sentence utils tests

--- src/shared/models/__mocks__/filterMocks.js
+++ src/shared/models/__mocks__/filterMocks.js
@@ -4,6 +4,8 @@ import { mockSetupResourceModels } from 'shared/testUtilities/index.js'
 
 const resourceModels = mockSetupResourceModels()
 
+// Grabs the resource model (src/shared/models/resources/*)
+// and hydrates a custom-search filter with it
 export const filterMocks = constant => {
   const resourceModel = resourceModels[constant]
   const filter = new Filter({
