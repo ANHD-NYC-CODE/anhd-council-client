375356f47ae38c0f519c1d9bcade891b866e04cf
Ross <ross_patton@protonmail.com>
2021-03-13 12:31:30 -0500

    add audit fix to build script, and test that css building works

--- pull_staging.sh
+++ pull_staging.sh
@@ -2,6 +2,7 @@ git checkout staging
 git checkout .
 git pull origin staging
 npm install --production --no-optional
+npm audit fix
 npm run build
 cd /var/www/anhd-council-backend
 sh deploy_nginx.sh
