e023eb6f27eccd80687ee933e7bbc2f018b82fcc
Ross <ross_patton@protonmail.com>
2021-03-14 11:33:09 -0400

    add publish step back

--- pull_staging.sh
+++ pull_staging.sh
@@ -3,13 +3,6 @@ git checkout .
 git pull origin staging
 npm install --production --no-optional
 npm audit fix
-
-rm -rf build/
-rm -rf build_publish/
-npm run build
-
-mkdir -p ./build_publish
-cp -r build/* build_publish
-
+npm run publish
 cd /var/www/anhd-council-backend
 sh deploy_nginx.sh
