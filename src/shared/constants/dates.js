36db8e81b157527dd9372c845de756d9f17b3d0a
Ross <ross_patton@protonmail.com>
2021-03-18 23:00:15 -0400

    fix a couple minor UI bugs with custom search

--- src/shared/constants/dates.js
+++ src/shared/constants/dates.js
@@ -5,15 +5,15 @@ export const DISTRICT_RESULTS_DATE_ONE = dayjs() // last 30 days
   .subtract(30, 'days')
   .format('YYYY-MM-DD')
 export const DISTRICT_RESULTS_DATE_TWO = dayjs() // 1 year ago jan 1st.
-  .subtract('1', 'year')
+  .subtract(1, 'year')
   .format('YYYY-MM-DD')
 export const DISTRICT_RESULTS_DATE_THREE = dayjs() // Three years jan 1st.
-  .subtract('3', 'year')
+  .subtract(3, 'year')
   .format('YYYY-MM-DD')
 
 // Custom Search
 export const CUSTOM_DEFAULT_END_DATE = dayjs().format('YYYY-MM-DD')
 
 export const CUSTOM_DEFAULT_START_DATE = dayjs()
-  .subtract(1, 'Y')
+  .subtract(1, 'year')
   .format('YYYY-MM-DD')
