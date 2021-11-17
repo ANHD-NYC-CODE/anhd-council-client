2a8c8b97aeb38f0f5cd52074177812943dc50c70
Gence Yalcin <genceyalcin@gmail.com>
2021-11-16 22:34:48 -0500

    User bookmarks in store

--- src/Store/MyDashboard/actions/index.js
+++ src/Store/MyDashboard/actions/index.js
@@ -1,3 +1,69 @@
-export const bookmarkProperty = (bbl, user) => {
+import { constructAxiosPost, constructAxiosDelete, constructAxiosGet } from "shared/utilities/Axios";
+import * as c from "../constants";
+import * as u from "shared/constants/urls"
 
+const handleBookmarkProperty = () => ({
+    type: c.BOOKMARK_PROPERTY
+})
+
+const handleUnBookmarkProperty = () => ({
+    type: c.UNBOOKMARK_PROPERTY
+})
+
+const handleGetUserBookmarkedProperties = (response) => ({
+    type: c.GET_USER_BOOKMARKED_PROPERTIES,
+    data: response.data,
+})
+
+export const bookmarkProperty = (bbl, name) => (dispatch, getState, access_token) => {
+    const requestId = Math.floor(Math.random() * 1000000)
+
+    return constructAxiosPost(
+        dispatch,
+        getState,
+        requestId,
+        u.BOOKMARKS_URL,
+        {
+            bbl, name
+        },
+        null,
+        access_token,
+        c.BOOKMARK_PROPERTY,
+        handleBookmarkProperty
+    )
 } 
+
+export const unBookmarkProperty = (id) => (dispatch, getState, access_token) => {
+    const requestId = Math.floor(Math.random() * 1000000)
+
+    return constructAxiosDelete(
+        dispatch,
+        getState,
+        requestId,
+        `${u.BOOKMARKS_URL}${id}`,
+        null,
+        null,
+        access_token,
+        c.UNBOOKMARK_PROPERTY,
+        handleUnBookmarkProperty
+    )
+} 
+
+export const getUserBookmarkedProperties = () =>  (dispatch, getState, access_token) => {
+    const requestId = Math.floor(Math.random() * 1000000);
+    
+    return constructAxiosGet(
+        dispatch,
+        getState,
+        requestId,
+        u.BOOKMARKS_URL,
+        null,
+        access_token,
+        c.GET_USER_BOOKMARKED_PROPERTIES,
+        handleGetUserBookmarkedProperties
+    )
+}
+
+export const clearUserBookmarkedProperties = () => ({
+    type: c.CLEAR_USER_BOOKMARKED_PROPERTIES
+})
