99ae3336779933cdb567a8e7525466b73aa75fe4
Gence Yalcin <genceyalcin@gmail.com>
2021-11-23 23:29:44 -0500

    Save search button in advanced search

--- src/AdvancedSearch/AdvancedSearchSentenceEditor/index.js
+++ src/AdvancedSearch/AdvancedSearchSentenceEditor/index.js
@@ -4,17 +4,31 @@ import PropTypes from 'prop-types'
 import { formatNumber } from 'shared/utilities/languageUtils'
 
 import { constructAdvancedSearchSentence } from 'shared/utilities/sentenceUtils'
+import { getCustomSearchPath } from 'shared/utilities/routeUtils'
 import { Button } from 'react-bootstrap'
 import { clearAdvancedSearchRequest } from 'Store/AppState/actions'
+import { requestWithAuth } from 'shared/utilities/authUtils'
+import { 
+  getUserSavedCustomSearches, 
+  deleteUserCustomSearch 
+} from 'Store/MyDashboard/actions'
 
 import ConfigContext from 'Config/ConfigContext'
 import ModalContext from 'Modal/ModalContext'
-
+import SaveSearchButton from 'shared/components/buttons/SaveSearchButton'
+import SaveOrEditCustomSearch from 'shared/components/modals/SaveCustomSearchModal'
 import './style.scss'
 
 const AdvancedSearchSentenceEditor = props => {
   const numberOfUnits = props.results.reduce((total, result) => parseInt(total) + parseInt(result['unitsres'] || 0), 0)
   const searchedFilters = [...props.advancedSearch.conditions[0].filters];
+  const loggedIn = !!props.user;
+  const thisUrl = getCustomSearchPath(
+    props.advancedSearch, 
+    props.geographyType, 
+    props.geographyId
+  );
+  const isSaved = !!props.savedSearches[thisUrl];
 
   const getFilterLabel = (filter) => {
     return filter.resourceModel.label
@@ -36,7 +50,52 @@ const AdvancedSearchSentenceEditor = props => {
     return constructAdvancedSearchSentence(props.advancedSearch, props.loading)
   }
 
+  const constructSavedSearchName = () => {
+    return constructSentenceEditor().substring(11);
+  }
+
+  const onSaveSearch = (e, modal) => {
+    if (!isSaved) {
+      e.preventDefault()
+      modal.setModal({
+        modalComponent: SaveOrEditCustomSearch,
+        modalProps: {
+          editing: false,
+          queryName: constructSavedSearchName(),
+          url: thisUrl
+        }
+      })
+    }
+    else {
+      const areYouSure = confirm("Are you sure you want to delete this bookmark?");
+      if (!areYouSure) return;
+
+      props.dispatch(requestWithAuth(
+        deleteUserCustomSearch(
+          props.savedSearches[thisUrl].id
+        )
+      )).then(() => {
+        props.dispatch(requestWithAuth(getUserSavedCustomSearches()));
+      });
+    }    
+  }
+
+  const openOCAModal = (e, modal, config) => {
+    e.preventDefault()
+    modal.setModal({
+      modalProps: {
+        title: config.infoModals["OCA_DATA_UNAVAILABLE"].title,
+        body: config.infoModals["OCA_DATA_UNAVAILABLE"].body,
+        sources: config.infoModals["OCA_DATA_UNAVAILABLE"].sources,
+        documentationBody: config.infoModals["OCA_DATA_UNAVAILABLE"].documentationBody,
+        documentationSources: config.infoModals["OCA_DATA_UNAVAILABLE"].documentationSources,
+        size: 'lg',
+      },
+    })
+  }
+
   return (
+    
     <div className="advanced-search-sentence-editor">
       <div className="advanced-search-sentence-editor__sentence-section">
         {constructSentenceEditor(
@@ -45,6 +104,20 @@ const AdvancedSearchSentenceEditor = props => {
           props.mapFilterDate,
           props.filterCondition
         )}
+        <ModalContext.Consumer>
+        {modal => {
+          return loggedIn && (
+            <div className="advanced-search-sentence-editor__search">
+              <SaveSearchButton 
+                active={isSaved} 
+                activeText = "You have saved this search"
+                text="Save this search" 
+                onSave={e => onSaveSearch(e, modal)}
+              />
+            </div>
+          )
+        }}
+        </ModalContext.Consumer>
       </div>
       {
         searchedFilters.some(filter => getFilterResourceConstant(filter) === "OCA_HOUSING_COURT") &&
@@ -57,34 +130,8 @@ const AdvancedSearchSentenceEditor = props => {
                     return (
                       <button
                         className="error-cta text-link"
-                        onClick={e => {
-                          e.preventDefault()
-                          modal.setModal({
-                            modalProps: {
-                              title: config.infoModals["OCA_DATA_UNAVAILABLE"].title,
-                              body: config.infoModals["OCA_DATA_UNAVAILABLE"].body,
-                              sources: config.infoModals["OCA_DATA_UNAVAILABLE"].sources,
-                              documentationBody: config.infoModals["OCA_DATA_UNAVAILABLE"].documentationBody,
-                              documentationSources: config.infoModals["OCA_DATA_UNAVAILABLE"].documentationSources,
-                              size: 'lg',
-                            },
-                          })
-                        }}
-                        onKeyDown={e =>
-                          spaceEnterKeyDownHandler(e, e => {
-                            e.preventDefault()
-                            modal.setModal({
-                              modalProps: {
-                                title: config.infoModals["OCA_DATA_UNAVAILABLE"].title,
-                                body: config.infoModals["OCA_DATA_UNAVAILABLE"].body,
-                                sources: config.infoModals["OCA_DATA_UNAVAILABLE"].sources,
-                                documentationBody: config.infoModals["OCA_DATA_UNAVAILABLE"].documentationBody,
-                                documentationSources: config.infoModals["OCA_DATA_UNAVAILABLE"].documentationSources,
-                                size: 'lg',
-                              },
-                            })
-                          })
-                        }
+                        onClick={e => openOCAModal(e, modal, config)}
+                        onKeyDown={e => openOCAModal(e, modal, config)}
                       >
                         Housing court case data not available for properties with fewer than 11 residential units. See why.
                       </button>
@@ -152,6 +199,10 @@ AdvancedSearchSentenceEditor.propTypes = {
   loadingButDisplayingForm: PropTypes.bool,
   displayingForm: PropTypes.bool,
   toggleForm: PropTypes.func,
+  user: PropTypes.object,
+  savedSearches: PropTypes.object,
+  geopraphyId: PropTypes.any,
+  geographyType: PropTypes.any
 }
 AdvancedSearchSentenceEditor.defaultProps = {
   results: [],
