544c2f8af26dfab9d7f226b4685d2fc2a2d7edd0
Gence Yalcin <genceyalcin@gmail.com>
2021-11-30 05:04:30 -0500

    Auth changes in district dash

--- src/shared/constants/elementCopy.js
+++ src/shared/constants/elementCopy.js
@@ -1,1 +1,2 @@
-export const LOGIN_CTA = 'Log in / Register to view'
+export const LOGIN_CTA = 'Log in with a trusted account to view'
+export const REQUEST_CTA = 'Request a trusted account to view'
