8c995dfc777327e5744a775b779d04f61d2a81b1
Scott Kutler <scottkutler@gmail.com>
2022-11-07 17:36:18 -0500

    new state senate map added on mapbox

--- src/LeafletMap/index.js
+++ src/LeafletMap/index.js
@@ -247,7 +247,7 @@ export default class LeafletMap extends React.PureComponent {
           {this.props.currentGeographyType === 'STATE_SENATE' && (
             <TileLayer
               attribution="mapbox"
-              url="https://api.mapbox.com/styles/v1/anhdnyc/ck30asbr30tk91cmmortl5u4e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5oZG55YyIsImEiOiJjanQ0ZWRqaDcxMmRxNDlsbHV1OXN0aGx6In0.i07oerfvXtcRfm3npws7mA?dt=nov7"
+              url="https://api.mapbox.com/styles/v1/anhdnyc/cla7cvnlq000h15o9oiftl65e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5oZG55YyIsImEiOiJjanQ0ZWRqaDcxMmRxNDlsbHV1OXN0aGx6In0.i07oerfvXtcRfm3npws7mA&dt"
             />
           )}
           {this.props.currentGeographyType === 'ZIPCODE' && (
