3f0c5f9904a652b047b756af1fc9ea62332524e4
Scott Kutler <scottkutler@gmail.com>
2024-07-02 11:52:40 -0400

    Update deploy_staging.sh

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,1 +1,1 @@
-ssh -t anhd@138.197.79.10 "cd /var/www/staging-anhd-council-client && sudo sh pull_staging.sh"
+ssh -t root@138.197.79.10 "cd /var/www/staging-anhd-council-client && sudo sh pull_staging.sh"
