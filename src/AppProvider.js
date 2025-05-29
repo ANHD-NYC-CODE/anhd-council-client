0961481606797bc21c2cbdab52e6627827e34edf
Scott Kutler <scottkutler@gmail.com>
2025-05-28 23:16:32 -0400

    Suppress SASS deprecation warnings in build process; verify build and publish

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import configureStore from 'Store/configureStore'
+import configureStore from './Store/configureStore'
 import { Provider } from 'react-redux'
-import { getUserStorageData } from 'shared/utilities/storageUtils'
-import App from 'App'
+import { getUserStorageData } from './shared/utilities/storageUtils'
+import App from './App'
 import ReactGA from 'react-ga'
-import * as c from 'shared/constants'
+import * as c from './shared/constants'
 if (c.ENABLE_GOOGLE_ANALYTICS) {
   ReactGA.initialize('G-3VVXDRFSS7', {
     debug: process.env.NODE_ENV === 'development',
